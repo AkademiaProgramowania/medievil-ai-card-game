63dad15b36221055e33cfb1b2051ae144af704ab
DESKTOP-EDNKSS0\Quane <zawartkawoj@gmail.com>
2023-03-07 18:22:46 +0100

    deleting Wojtek sout

--- src/main/java/medevil/ai/Main.java
+++ src/main/java/medevil/ai/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("hello");
-        System.out.println("Wojtek");
     }
 }
